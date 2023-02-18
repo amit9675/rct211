0a95d0990774aad368c6d0858cd9f9028e941156
Amit bhandari <a0423355@gmail.com>
2023-02-18 13:56:22 +0530

    rct211

---
+++ cypress/src/App.js
@@ -0,0 +1,17 @@
+
+import { useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [counter,setCounter] = useState(10)
+  return (
+    <div className="App">
+    <h1 className='counter_value'>Counter: {counter}</h1>
+    <button className='add_btn'  onClick={()=>setCounter(counter+1)}>Add</button>
+    <button className='reduce_btn' onClick={()=>setCounter(counter-1)}>Reduce</button>
+    </div>
+  );
+}
+
+export default App;
